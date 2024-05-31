17b8bc3ce076194a4b097d597c5d51e700815ebe
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-05-31 17:07:19 +0700

    Create Level.2.ts

---
+++ Data Types/Level.2.ts
@@ -0,0 +1,87 @@
+//Cau 1:
+// console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another')
+
+//Cau 2:
+// console.log('"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, si don\'t just give money but reach out your hand instead."')
+
+//Cau 3:
+// function equal(a: string, b: number) {
+//     if (typeof a == typeof b) {
+//         return true
+//     }
+//     else {
+//         return `typeof ${+a}: ${typeof +a}`
+//     }
+// }
+
+// console.log(equal('10', 10))
+
+//Cau 4:
+// function equal(a: string, b: number) {
+//     if (parseFloat(a) == b) {
+//         return true
+//     }
+//     else {
+//         return Math.ceil(parseFloat(a))
+//     }
+// }
+
+// console.log(equal('9.8', 10))
+
+//Cau 5:
+// let short_1: string = 'python'
+// let short_2: string = 'jargon'
+
+// function findOn(a: string, b: string) {
+//     if (a.search('on') >= 0 && b.search('on') >= 0) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(findOn(short_1, short_2))
+
+//Cau 6:
+// let course: string = 'I hope this course is not full jargon'
+
+// function findJargon(a: string) {
+//     if (a.search('jargon') >= 0) {
+//         return true
+//     }
+//     else {
+//         false
+//     }
+// }
+
+// console.log(findJargon(course))
+
+//Cau 7:
+// let randNum: number = Math.random() * 100
+
+// console.log(Math.round(randNum))
+
+//Cau 8:
+// let randNum: number = (Math.random() + 1) * 50
+
+// console.log(Math.round(randNum))
+
+//Cau 9:
+// let randNum: number = Math.random() * 255
+
+// console.log(Math.round(randNum))
+
+//Cau 10:
+// let language: string = 'JavaScript'
+// let randNum: number = Math.random() * (language.length - 1)
+
+// console.log(language.charAt(Math.round(randNum)))
+
+//Cau 11:
+// console.log(`1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n 5 1 5 25 125`)
+
+//Cau 12:
+// let long: string = 'You cannot end a sentence with because because because is a conjunction'
+
+// console.log(long.substring(31, 54))
