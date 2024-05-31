f0ba3a89138267f6d1f120ed03c3dddeaafd9af0
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-05-31 17:15:41 +0700

    Create Level.1.ts

---
+++ Booleans, Operators, Date/Level.1.ts
@@ -0,0 +1,197 @@
+//Cau 1:
+// var firstName: string = 'Cao'
+// var lastName: string = 'Ha'
+// var country: string = 'Viet Nam'
+// var city: string = 'Tp.HCM'
+// var age: number = 22
+// var isMarried: boolean = false
+// var year: number = 2002
+
+// console.log(typeof firstName)
+// console.log(typeof lastName)
+// console.log(typeof country)
+// console.log(typeof city)
+// console.log(typeof age)
+// console.log(typeof isMarried)
+// console.log(typeof year)
+
+//Cau 2:
+// var str: string = '10'
+// var num: number = 10
+
+// console.log(`the anwser is always false.`)
+
+//Cau 3:
+// console.log(parseInt('9.8') == 10)
+
+//Cau 4-i:
+// if (10) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// if ('TypeScript') {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// if (true) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+//Cau 4-ii:
+// if (null) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// if (undefined) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+// if (NaN) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+//Cau 5-i:
+// console.log(true)
+// console.log(4 > 3)
+
+//Cau 5-ii:
+// console.log(true)
+// console.log(4 >= 3)
+
+//Cau 5-iii:
+// console.log(false)
+// console.log(4 < 3)
+
+//Cau 5-iv:
+// console.log(false)
+// console.log(4 <= 3)
+
+//Cau 5-v:
+// console.log(true)
+// console.log(4 == 4)
+
+//Cau 5-vi:
+// console.log(true)
+// console.log(4 === 4)
+
+//Cau 5-vii:
+// console.log(false)
+// console.log(4 != 4)
+
+//Cau 5-viii:
+// console.log(false)
+// console.log(4 !== 4)
+
+//Cau 5-ix:
+// console.log(false)
+
+//Cau 5-x:
+// console.log(false)
+
+//Cau 5-xi:
+// console.log(false)
+
+//Cau 5-xii:
+// var language_1: string = 'python'
+// var language_2: string = 'jargon'
+
+// console.log(language_1.length != language_2.length)
+
+//Cau 6-i:
+// console.log(true)
+// console.log(4 > 3 && 10 < 12)
+
+//Cau 6-ii:
+// console.log(false)
+// console.log(4 > 3 && 10 > 12)
+
+//Cau 6-iii:
+// console.log(true)
+// console.log(4 > 3 || 10 < 12)
+
+//Cau 6-iv:
+// console.log(true)
+// console.log(4 > 3 || 10 > 12)
+
+//Cau 6-v:
+// console.log(false)
+// console.log(!(4 > 3))
+
+//Cau 6-vi:
+// console.log(true)
+// console.log(!(4 < 3))
+
+//Cau 6-vii:
+// console.log(true)
+// console.log(!false)
+
+//Cau 6-viii:
+// console.log(false)
+// console.log(!(4 > 3 && 10 < 12))
+
+//Cau 6-ix:
+// console.log(true)
+// console.log(!(4 > 3 && 10 > 12))
+
+//Cau 6-x:
+// console.log(true)
+
+//Cau 6-xi:
+// var language: string = 'python'
+// var legend: string = 'dragon'
+
+// console.log(!language.includes('on'))
+// console.log(!legend.includes('on'))
+
+//Cau 7-i:
+// const now = new Date()
+
+// console.log(now.getFullYear())
+
+//Cau 7-ii:
+// const now = new Date()
+
+// console.log(now.getMonth() + 1)
+
+//Cau 7-iii:
+// const now = new Date()
+
+// console.log(now.getDay())
+
+//Cau 7-iv:
+// const now = new Date()
+
+// console.log(now.getDate())
+
+//Cau 7-v:
+// const now = new Date()
+
+// console.log(now.getHours())
+
+//Cau 7-vi:
+// const now = new Date()
+
+// console.log(now.getMinutes())
+
+//Cau 7-vii:
+// const allSeconds = Date.now()
+
+// console.log(allSeconds)
