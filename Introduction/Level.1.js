66c1129f82b23e2e82fd4306bccb1bddbf951906
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-05-31 16:58:02 +0700

    Create Level.1.js

---
+++ Introduction/Level.1.js
@@ -0,0 +1,48 @@
+//Cau 1:
+//  Nhan xet co the lam cho ma co the doc duoc
+
+//Cau 2:
+//  Chao mung ban den voi 30DaysOfJavaScript
+
+//Cau 3:
+//  Nhan xet co the lam cho ma de doc, de su dung lai va nhieu thong tin
+
+//cau 4:
+// var a = ''
+// var b = true
+// var c = undefined
+// var d = null
+
+//Cau 5:
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+
+//Cau 6:
+// var a
+// var b
+// var c
+// var d
+
+//Cau 7:
+// var a = 1
+// var b = 2
+// var c = 3
+// var d = 4
+
+//Cau 8:
+// var fullName = 'Cao Hai Ha'
+// var marry = 'doc than'
+// var country = 'Viet Nam'
+// var age = 22
+
+//Cau 9:
+// var fullName = 'Cao Hai Ha'; var marry = 'docthan'; var country = 'Viet Nam'; var age = 22
+
+//Cau 10:
+// var myAge = 22
+// var yourAge = 30
+
+// console.log(`I am ${myAge} years old.`)
+// console.log(`You are ${yourAge} years old.`)
