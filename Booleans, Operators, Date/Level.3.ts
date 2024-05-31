af6180ce2dd53bfc5881092ba4b5fef1cb98bdb0
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-05-31 17:21:31 +0700

    Create Level.3.ts

---
+++ Booleans, Operators, Date/Level.3.ts
@@ -0,0 +1,9 @@
+//Cau 1:
+// let now: any = new Date()
+// var y: any = now.getFullYear()
+// var m: string = String(now.getMonth() + 1).padStart(2, '0')
+// var d: string = String(now.getDate()).padStart(2, '0')
+// var h: string = String(now.getHours()).padStart(2, '0')
+// var min: string = String(now.getMinutes()).padStart(2, '0')
+
+// console.log(`${y}-${m}-${d} ${h}: ${min}`)
