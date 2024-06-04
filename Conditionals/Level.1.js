2a4c4c7ac7b2103c1dcd620c7b2a85f298368496
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-06-04 08:55:07 +0700

    day4/level1/js/updated

---
+++ Conditionals/Level.1.js
@@ -0,0 +1,52 @@
+//Cau 1:
+// var yourAge = prompt('Enter your age')
+
+// console.log(`Enter your age: ${yourAge}`)
+// if (+yourAge >= 18) {
+//     console.log(`You are old enough to drive.`)
+// }
+// else {
+//     console.log(`You are left with ${18 - +yourAge} years to drive.`)
+// }
+
+//Cau 2:
+// let myAge = 22
+// var yourAge = prompt('Enter your age')
+
+// console.log(`Enter your age: ${yourAge}`)
+// if (myAge > +yourAge) {
+//     console.log(`You are ${myAge - +yourAge} years younger than me.`)
+// }
+// else if (myAge == yourAge) {
+//     console.log('We are the same age.')
+// }
+// else {
+//     console.log(`You are ${+yourAge - myAge} years older than me.`)
+// }
+
+//Cau 3:
+// let a = 4
+// let b = 3
+// let equal = a > b
+
+// if (a > b) {
+//     console.log('a is greater than b')
+// }
+// else {
+//     console.log('a is less than b')
+// }
+
+// equal
+//     ? console.log('a is greater than b')
+//     : console.log('a is less than b')
+
+//Cau 4:
+// var num = prompt('Enter a number')
+
+// console.log(`Enter a number: ${num}`)
+// if (num % 2 == 0) {
+//     console.log(`${num} is an even number.`)
+// }
+// else {
+//     console.log(`${num} is an odd number.`)
+// }
