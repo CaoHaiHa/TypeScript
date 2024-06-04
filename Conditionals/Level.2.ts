c74333b81f337304fbce8bef58b3abed13811ebe
CaoHaiHa <104907705+CaoHaiHa@users.noreply.github.com>
2024-06-04 08:59:12 +0700

    day4/level2/ts/updated

---
+++ Conditionals/Level.2.ts
@@ -0,0 +1,59 @@
+//Cau 1:
+// var score: any = prompt('Enter your score')
+
+// console.log(`Enter your score: ${score}`)
+// if (score >= 80 && score <= 100) {
+//     console.log('Your grade is: A')
+// }
+// else if (score >= 70 && score <= 79) {
+//     console.log('Your grade is: B')
+// }
+// else if (score >= 60 && score <= 69) {
+//     console.log('Your grade is: C')
+// }
+// else if (score >= 50 && score <= 59) {
+//     console.log('Your grade is: D')
+// }
+// else {
+//     console.log('Your grade is: F')
+// }
+
+//Cau 2:
+// var m: any = prompt('Enter a month')
+
+// m = m.toLowerCase()
+// console.log(`Enter a month ${m}`)
+
+// if (m == 'september' || m == 'october' || m == 'november') {
+//     console.log(`The season is Autumn.`)
+// }
+// else if (m == `december` || m == `january` || m == `february`) {
+//     console.log(`The season is Winter.`)
+// }
+// else if (m == `march` || m == `april` || m == `may`) {
+//     console.log(`The season is Spring.`)
+// }
+// else if (m == `june` || m == `july` || m == `august`) {
+//     console.log(`The season is Summer.`)
+// }
+// else {
+//     console.log(`Entered value was not a month`)
+// }
+
+// //Cau 3:
+// var d: any = prompt(`Enter a day`)
+
+// console.log(`What is the day today? ${d}`)
+// d = d.toLowerCase()
+
+// if (d == `monday` || d == `tuesday` || d == `wednesday` || d == `thursday` || d == `friday`) {
+//     d = d.charAt(0).toUpperCase() + d.slice(1)
+//     console.log(`${d} is a working day.`)
+// }
+// else if (d == `saturday` || d == `sunday`) {
+//     d = d.charAt(0).toUpperCase() + d.slice(1)
+//     console.log(`${d} is a weekend.`)
+// }
+// else {
+//     console.log(`Entered value was not a day`)
+// }
