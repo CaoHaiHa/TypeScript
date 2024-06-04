3d1ff9c67728e44893b67ff617b20f12e2bed548
Cao Hai Ha <ha.cao-hai@internal.banvien.com>
2024-06-04 17:40:15 +0700

    var,let and const/updated

--- variable.js
+++ variable.js
@@ -13,7 +13,18 @@ a = 'name' //Có thể cập nhật giá trị
 
 console.log(a) //Trả về 'name'
 console.log(typeof a) //Kiểu dữ liệu trả về string
-//Kết luận: var khá linh hoạt vì có thể khai báo và cập nhật.
+
+//Updated
+
+console.log(x) //Trả về undefined vì var được đẩy lên đầu scope(Global scope) và gán giá trị undefined
+var x = 'Hello'
+
+if (true) {
+    var x = 'Hi'
+}
+console.log(x) //Trả về 'Hi'. Var có phạm vi trong Global scope và không bị giới hạn trong Block scope({})
+
+//Kết luận: Var hoạt động trong phạm vi Global scope. Có thể khai báo lại và cập nhật. Hosting sẽ đẩy var lên đầu scope được khởi tạo và gán giá trị undefined
 console.log('------------------------------------------------------------')
 
 //Let
@@ -33,7 +44,20 @@ c = 'name' //Có thể cập nhật giá trị
 
 console.log(c) //Trả về 'name'
 console.log(typeof c) //Kiểu dữ liệu trả về string
-//Kết luận: let linh hoạt nhưng không linh hoạt bằng var vì không thể khai báo lại.
+
+//Updated
+
+// console.log(y) //Giống var hosting của let đẩy let lên đầu scope và khai báo nhưng không gán giá trị
+let y = 'Hello'
+
+if (true) {
+    let y = 'Hi'
+    console.log(y) //Trả về 'Hi'
+}
+
+console.log(y) //Trả về 'Hello'. Vì let chỉ có thể hoạt động trong phạm vi Block scope({})
+
+//Kết luận: Let hoạt động trong phạm vi Block scope. Không thể khai báo lại nhưng có thể cập nhật. Hoisting của let sẽ đẩy let lên đâu scope nhưng không được khởi tạo.
 console.log('------------------------------------------------------------')
 
 //Const
@@ -48,3 +72,15 @@ console.log(typeof d) //Kiểu dữ liệu trả về number
 // const d = 15 //Không thể khai báo lại. Báo lỗi: SyntaxError: Identifier 'd' has already been declared
 
 // d = 'name' //Không thể cập nhật giá trị. Báo lỗi: TypeError: Assignment to constant variable
+
+//Updated
+// console.log(z) //Giống như let hoitsting của const đẩy nó lên đầu scope nhưng không khởi tạo.
+const z = 'Hello'
+
+if (true) {
+    const z = 'Hi'
+    console.log(z) //Trả về 'Hi'. Giống như let const cũng có phạm vi hoạt động trong Block scope
+}
+console.log(z) //Trả về 'Hello'
+
+//Kết luận: const hoạt động trong phạm vi Block scope. Const không thể khai báo lại và cập nhật. Hoisting của const sẽ đẩy nó lên đầu scope nhưng không khởi tạo.
