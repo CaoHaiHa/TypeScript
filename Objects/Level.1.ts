0c995021bbeeafd47305933b59a350595c6b05fc
Cao Hai Ha <ha.cao-hai@internal.banvien.com>
2024-06-05 10:23:49 +0700

    Object/Level 1/Updated

---
+++ Objects/Level.1.ts
@@ -0,0 +1,33 @@
+// //Cau 1:
+// type Dog = {
+//     [key: string]: any
+// }
+
+// const dog: Dog = {
+
+// }
+
+// //Cau 2:
+// console.log(dog)
+
+// //Cau 3:
+// dog.name = 'white'
+// dog.legs = 4
+// dog.color = 'black'
+// dog.age = 5
+// dog.bark = function () { return 'woof woof' }
+
+// console.log(dog)
+
+// //Cau 4:
+// let values: any[] = Object.values(dog)
+
+// console.log(values)
+
+// //Cau 5:
+// dog.breed = 'Bulldog'
+// dog.getInfo = function () {
+//     return `The dog's name is ${this.name}.\nHe has ${this.legs} legs.\nHe's ${this.color}.\nHe's ${this.age} years old.\nHe's ${this.breed}.\nAnd he ${this.bark()}.`
+// }
+
+// console.log(dog.getInfo())
