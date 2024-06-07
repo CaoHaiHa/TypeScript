b27bfc6ad50f2f0d4ae697fbc4513932f0107eac
Cao Hai Ha <ha.cao-hai@internal.banvien.com>
2024-06-07 16:04:44 +0700

    feature/implement/destructuring and spreading/Level 1/done

---
+++ Destructuring and Spreading/Level.1.js
@@ -0,0 +1,25 @@
+// //Array and Object:
+// const constants = [2.72, 3.14, 9.81, 37, 100]
+// const countries = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+// const rectangle = {
+//     width: 20,
+//     height: 10,
+//     area: 200,
+//     perimeter: 60
+// }
+
+// //Cau 1:
+// let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+
+// console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp)
+// console.log(`------------------------------------------------------------`)
+
+// //Cau 2:
+// let [fin, est, sw, den, nor] = countries
+// console.log(fin, est, sw, den, nor)
+// console.log(`------------------------------------------------------------`)
+
+// //Cau 3:
+// let { area, perimeter, width, height } = rectangle
+
+// console.log(area, perimeter, width, height)
---
+++ Destructuring and Spreading/Level.1.ts
@@ -0,0 +1,31 @@
+// //Array and Object:
+// const constants: number[] = [2.72, 3.14, 9.81, 37, 100]
+// const countries: string[] = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+// interface Rectangle {
+//     width: number,
+//     height: number,
+//     area: number,
+//     perimeter: number
+// }
+// const rectangle: Rectangle = {
+//     width: 20,
+//     height: 10,
+//     area: 200,
+//     perimeter: 60
+// }
+
+// //Cau 1:
+// let [e, pi, gravity, humanBodyTemp, waterBoilingTemp]: number[] = constants
+
+// console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp)
+// console.log(`------------------------------------------------------------`)
+
+// //Cau 2:
+// let [fin, est, sw, den, nor]: string[] = countries
+// console.log(fin, est, sw, den, nor)
+// console.log(`------------------------------------------------------------`)
+
+// //Cau 3:
+// let { area, perimeter, width, height }: Rectangle = rectangle
+
+// console.log(area, perimeter, width, height)
