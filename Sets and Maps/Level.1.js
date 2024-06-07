f800098f3eb459a638f84583ac0a324fb341addc
Cao Hai Ha <ha.cao-hai@internal.banvien.com>
2024-06-07 14:00:00 +0700

    feature/implement/Sets and Maps/Level 1/done

---
+++ Sets and Maps/Level.1.js
@@ -0,0 +1,42 @@
+// //Cau 1:
+// const emptySet = new Set()
+
+// console.log(emptySet)
+// console.log(`---------------------------------------------------------------`)
+
+// //Cau 2:
+// const numberSet = new Set()
+
+// for (let i = 0; i <= 10; i++) numberSet.add(i)
+
+// console.log(numberSet)
+// console.log(`---------------------------------------------------------------`)
+
+// //Cau 3:
+// numberSet.delete(5)
+// numberSet.delete(7)
+// numberSet.delete(9)
+
+// console.log(numberSet)
+// console.log(`---------------------------------------------------------------`)
+
+// //Cau 4:
+// numberSet.clear()
+
+// console.log(numberSet)
+// console.log(`---------------------------------------------------------------`)
+
+// //Cau 5:
+// const randomString = ['Daerxg', 'XpoDZH', 'qklNWB', 'GfKFwX', 'KWFkRR']
+// const setOfString = new Set(randomString)
+
+// console.log(setOfString)
+// console.log(`---------------------------------------------------------------`)
+
+// //Cau 6:
+// const countries = ['Finland', 'Sweden', 'Norway']
+
+// const mapCountries = new Map()
+// for (let i in countries) mapCountries.set(countries[i], countries[i].length)
+
+// console.log(mapCountries)
---
+++ Sets and Maps/Level.1.ts
@@ -0,0 +1,42 @@
+// //Cau 1:
+// const emptySet: Set<unknown> = new Set();
+
+// console.log(emptySet);
+// console.log(`---------------------------------------------------------------`);
+
+// //Cau 2:
+// const numberSet: Set<number> = new Set();
+
+// for (let i: number = 0; i <= 10; i++) numberSet.add(i);
+
+// console.log(numberSet);
+// console.log(`---------------------------------------------------------------`);
+
+// //Cau 3:
+// numberSet.delete(5);
+// numberSet.delete(7);
+// numberSet.delete(9);
+
+// console.log(numberSet);
+// console.log(`---------------------------------------------------------------`);
+
+// //Cau 4:
+// numberSet.clear();
+
+// console.log(numberSet);
+// console.log(`---------------------------------------------------------------`);
+
+// //Cau 5:
+// const randomString: string[] = ['Daerxg', 'XpoDZH', 'qklNWB', 'GfKFwX', 'KWFkRR'];
+// const setOfString: Set<string> = new Set(randomString);
+
+// console.log(setOfString);
+// console.log(`---------------------------------------------------------------`);
+
+// //Cau 6:
+// const countries: string[] = ['Finland', 'Sweden', 'Norway'];
+
+// const mapCountries: Map<string, number> = new Map();
+// for (let country of countries) mapCountries.set(country, country.length);
+
+// console.log(mapCountries);
